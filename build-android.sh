177a97d4d341065d69b9a3505009798511c7d5d9
Adam-VisualVocal <adam@visualvocal.com>
2019-08-08 14:10:46 -0700

    Update Android build script to use NDK R20 and Android API 28.

--- build-android.sh
+++ build-android.sh
@@ -4,14 +4,18 @@ SOURCE_DIRECTORY="$(dirname $0)"
 cd ${SOURCE_DIRECTORY}
 PWD=`pwd`
 SOURCE_DIRECTORY="${PWD}"
-BUILD_DIRECTORY="${SOURCE_DIRECTORY}/build/android"
 
-NDK_PATH="${SOURCE_DIRECTORY}/../ndk/android-ndk-r18b"
-TOOLCHAIN="${SOURCE_DIRECTORY}/../ndk/toolchains/r18b-arm-24"
-SYSROOT="${TOOLCHAIN}/sysroot"
 BUILD_PLATFORM="linux-x86_64"
+NDK_PATH="${SOURCE_DIRECTORY}/../ndk/android-ndk-r20"
+SYSROOT="${TOOLCHAIN}/sysroot"
 TOOLCHAIN_VERSION="4.9"
-ANDROID_VERSION="24"
+ANDROID_VERSION="28"
+
+#
+# ARMv7 (32-bit)
+#
+BUILD_DIRECTORY="${SOURCE_DIRECTORY}/build/android/armeabi-v7a"
+TOOLCHAIN="${SOURCE_DIRECTORY}/../ndk/toolchains/r20-arm-28"
 
 mkdir -p ${BUILD_DIRECTORY}
 cd ${BUILD_DIRECTORY}
@@ -21,6 +25,29 @@ cmake -G"Unix Makefiles" \
   -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
   -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
   -DCMAKE_ASM_FLAGS="--target=arm-linux-androideabi${ANDROID_VERSION}" \
+  -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
+  -DCMAKE_INSTALL_PREFIX=install \
+  -DENABLE_STATIC=TRUE -DENABLE_SHARED=FALSE \
+  -DWITH_SIMD=TRUE \
+  ${SOURCE_DIRECTORY}
+cmake --build .
+cmake --build . --target install/strip
+
+
+#
+# ARMv8 (64-bit)
+#
+BUILD_DIRECTORY="${SOURCE_DIRECTORY}/build/android/arm64-v8a"
+TOOLCHAIN="${SOURCE_DIRECTORY}/../ndk/toolchains/r20-arm64-28"
+
+mkdir -p ${BUILD_DIRECTORY}
+cd ${BUILD_DIRECTORY}
+cmake -G"Unix Makefiles" \
+  -DANDROID_ABI=arm64-v8a \
+  -DANDROID_ARM_MODE=arm \
+  -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
+  -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
+  -DCMAKE_ASM_FLAGS="--target=aarch64-linux-android${ANDROID_VERSION}" \
   -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
   -DCMAKE_INSTALL_PREFIX=install \
   -DENABLE_STATIC=TRUE -DENABLE_SHARED=FALSE \
